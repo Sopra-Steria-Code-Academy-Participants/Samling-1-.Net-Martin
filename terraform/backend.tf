3847089ae924132cacbed037b328cc190effb7fb
Martin <marobe@equinor.com>
2025-03-04 18:52:06 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "TEST_DEV"
     storage_account_name = "terraformcodeacademy"
-    container_name       = "tfstate"
+    container_name       = "tfstate30"
     key                  = "terraform.tfstate30"
     subscription_id      = "e25e96db-c54e-4251-bed2-5d7a3e52fc4e"
     tenant_id            = "8b87af7d-8647-4dc7-8df4-5f69a2011bb5"
